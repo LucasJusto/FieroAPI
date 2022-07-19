0cfb7be317ded954d7bb46f2715a915b47e2da7f
Marcelo Diefenbach <58398373+MarceloDiefenbach@users.noreply.github.com>
2022-07-19 17:53:10 -0300

    Bringing createTables back

---
+++ src/database/migrations/1656705416812-CreateTables.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateTables1656705416812 implements MigrationInterface {
+    name = 'CreateTables1656705416812'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('CREATE TABLE "user" ("id" uuid NOT NULL, "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "created_At" TIMESTAMP NOT NULL DEFAULT now(), "updated_At" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))');
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('DROP TABLE "user"');
+    }
+
+}
